1dbe0a60b1910f5ee5e9e0635bf8696d3c0e2ea6
MCOS12333 <silvetersmanels@gmail.com>
2025-08-08 14:53:24 +0100

    Update Mneis.lua

--- Mneis.lua
+++ Mneis.lua
@@ -1,15 +1,21 @@
 task.spawn(function()
     script_key = "pMtaTJiZhDgHTSmJcOFHmuhsGZXCbqNg"
     getgenv().Config = {
-        ["PetFarmAutoSwitchFullGrown"] = false,
-        ["PetFarmActive"] = true,
-        ["EggFarmActive"] = false,
-        ["Blur_username"] = true,
-        ["Blazing_Lion_Log"] = false,
-        ["DiscordId"] = "123456",
-        ["Webhook"] = "https://discord.com/api/webhooks/123456",
-    }
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+    ["PetAMountToBuy"] = "0", -- can be 999
+    ["PetRemoteToBuy"] = "moon_2025_egg",
+
+    ["PetFarmAutoSwitchFullGrown"] = false,
+    ["PetFarmActive"] = false,
+    ["LitePetFarmActive"] = true,
+    ["EggFarmActive"] = true,
+    
+    ["HideUselessGui"] = true,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "",
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
 end)
 wait(10)
 loadstring(game:HttpGet("https://pastebin.com/61xpUAWX"))()
