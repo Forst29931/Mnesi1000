49012372c01e67c9d17e86e65e70331692e8ad18
MCOS12333 <silvetersmanels@gmail.com>
2025-11-10 13:59:39 +0000

    Update 10to20.lua

--- 10to20.lua
+++ 10to20.lua
@@ -1,18 +1,26 @@
 task.spawn(function()
     script_key="ooVNbfcdHaVoNAkFFoQmqzRlJdutqxKl";
     getgenv().Config = {
-        ["PetAMountToBuy"] = "99", -- can be 999
+        ["PetAMountToBuy"] = "0", -- can be 999
         ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
         ["PetRemoteToBuy"] = "retired_egg", -- exemple for gifts halloween_2025_spider_box
 
+
         ["PetFarmAutoSwitchFullGrown"] = false,
         ["PetFarmActive"] = true, -- farm potion
-        ["EggFarmActive"] = false, -- farm egg, if no more eggs will farm potion
+
+
+        ["EggFarmActive"] = false, -- farm egg, if no more eggs will farm potion, if found new egg will farm them to
+        ["EggToIgnore"] = {"pet_recycler_2025_basic_egg","pet_recycler_2025_crystal_egg"}, -- eggs to ignore when eggfarm active
+        ["LoopBuyEgg"] = false, -- when enough bucks will buy egg, only work with eggfarm
+        ["EggToBuyEgg"] = "retired_egg",
+
 
         ["NeonMegaFarm"] = false,
         ["BuyEgg"] = "retired_egg", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
         
-        ["AutoReleasePet"] = false,
+
+        ["AutoReleasePet"] = false, -- work with egg/pet farm, will release selected rarity
         ["RecycleWebhook"] = "",
         ["common"] = false,
         ["uncommon"] = false,
