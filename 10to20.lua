ade8fe66d8a200673c19437ef143971d45df398f
MCOS12333 <silvetersmanels@gmail.com>
2025-12-23 20:47:20 +0000

    Refactor buying logic in 10to20.lua

--- 10to20.lua
+++ 10to20.lua
@@ -1,39 +1,14 @@
 task.spawn(function()
-    script_key="ooVNbfcdHaVoNAkFFoQmqzRlJdutqxKl";
-    getgenv().Config = {
-        ["PetAMountToBuy"] = "0", -- can be 999
-        ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
-        ["PetRemoteToBuy"] = "retired_egg", -- exemple for gifts halloween_2025_spider_box
+    task.wait(10)
+    for i = 1,99 do task.wait()
+        local args = {
+            [1] = "pets",
+            [2] = "retired_egg",
+            [3] = {
+                ["buy_count"] = 99
+            }
+        }
 
-
-        ["PetFarmAutoSwitchFullGrown"] = false,
-        ["PetFarmActive"] = true, -- farm potion
-
-
-        ["EggFarmActive"] = false, -- farm egg, if no more eggs will farm potion, if found new egg will farm them to
-        ["EggToIgnore"] = {"pet_recycler_2025_basic_egg","pet_recycler_2025_crystal_egg"}, -- eggs to ignore when eggfarm active
-        ["LoopBuyEgg"] = false, -- when enough bucks will buy egg, only work with eggfarm
-        ["EggToBuyEgg"] = "retired_egg",
-
-
-        ["NeonMegaFarm"] = false,
-        ["BuyEgg"] = "retired_egg", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
-        
-
-        ["AutoReleasePet"] = false, -- work with egg/pet farm, will release selected rarity
-        ["RecycleWebhook"] = "",
-        ["common"] = false,
-        ["uncommon"] = false,
-        ["rare"] = false,
-        ["ultra_rare"] = false,
-
-        ["HideUselessGui"] = true,
-        ["Blur_username"] = true,
-        ["Blazing_Lion_Log"] = false,
-        ["DiscordId"] = "123456",
-        ["Webhook"] = "",
-        ["LegendaryWebhook"] = "",
-        ["NeonMegaWebhook"] = "",
-    }
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+        game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("ShopAPI/BuyItem"):InvokeServer(unpack(args))
+    end
 end)
